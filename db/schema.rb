5d50dfc783c2ae3bb33d4aa56d6605dbc943c4b3
Leo de Nevi <leonardo.denevi@greenstatuspro.com>
2013-02-10 01:54:43 -0500

    Tasks have executors instead of owners, review point to one purchased template; comments can be attached to any object; organizations have (placeholder) governing body reference

--- db/schema.rb
+++ db/schema.rb
@@ -27,56 +27,66 @@ ActiveRecord::Schema.define(:version => 20130207201638) do
   create_table "comments", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.integer  "author_id"
     t.integer  "attachments_count"
-    t.integer  "attachable_id"
-    t.string   "attachable_type"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+    t.integer  "author_id"
   end
 
   add_index "comments", ["author_id"], :name => "index_comments_on_author_id"
   add_index "comments", ["body"], :name => "index_comments_on_body"
+  add_index "comments", ["title"], :name => "index_comments_on_title"
 
   create_table "organizations", :force => true do |t|
     t.string   "name"
     t.string   "display_name"
-    t.integer  "region_id"
+    t.integer  "governing_law_id"
+    t.integer  "owner_id"
     t.integer  "parent_id"
     t.integer  "root_parent_id"
-    t.boolean  "is_leaf",        :default => false
-    t.boolean  "is_branch",      :default => false
-    t.integer  "owner_id"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.boolean  "is_branch",        :default => false
+    t.boolean  "is_leaf",          :default => false
+    t.datetime "created_at",                          :null => false
+    t.datetime "updated_at",                          :null => false
   end
 
   add_index "organizations", ["display_name"], :name => "index_organizations_on_display_name"
+  add_index "organizations", ["governing_law_id"], :name => "index_organizations_on_governing_law_id"
   add_index "organizations", ["is_branch"], :name => "index_organizations_on_is_branch"
   add_index "organizations", ["is_leaf"], :name => "index_organizations_on_is_leaf"
   add_index "organizations", ["name"], :name => "index_organizations_on_name"
   add_index "organizations", ["parent_id"], :name => "index_organizations_on_parent_id"
-  add_index "organizations", ["region_id"], :name => "index_organizations_on_region_id"
   add_index "organizations", ["root_parent_id"], :name => "index_organizations_on_root_parent_id"
 
   create_table "purchased_templates", :force => true do |t|
     t.integer  "agency_id"
     t.string   "agency_display_name"
-    t.integer  "approved_by_id"
-    t.datetime "approved_at"
-    t.string   "full_name"
+    t.text     "description"
     t.string   "display_name"
     t.string   "frequency"
-    t.text     "description"
+    t.string   "full_name"
     t.text     "objectives"
-    t.text     "tasks"
-    t.string   "regulatory_review_name"
     t.integer  "organization_id"
+    t.string   "regulatory_review_name"
+    t.integer  "template_id"
     t.integer  "revision"
     t.boolean  "is_latest_revision"
+    t.text     "tasks"
+    t.boolean  "is_archived"
+    t.integer  "approved_by_id"
+    t.datetime "approved_at"
+    t.integer  "modified_by_id"
     t.integer  "purchased_by_id"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
+    t.integer  "shared_by_id"
+    t.datetime "shared_at"
+    t.integer  "parent_id"
+    t.integer  "root_parent_id"
+    t.boolean  "is_branch",              :default => false
+    t.boolean  "is_leaf",                :default => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
   end
 
   add_index "purchased_templates", ["approved_by_id"], :name => "index_purchased_templates_on_approved_by_id"
@@ -86,17 +96,17 @@ ActiveRecord::Schema.define(:version => 20130207201638) do
   add_index "purchased_templates", ["purchased_by_id"], :name => "index_purchased_templates_on_purchased_by_id"
 
   create_table "reviews", :force => true do |t|
-    t.string   "name"
     t.integer  "owner_id"
     t.string   "frequency"
-    t.string   "status"
-    t.integer  "purchased_template_id"
+    t.string   "name"
     t.integer  "organization_id"
+    t.integer  "purchased_template_id"
+    t.string   "status"
+    t.datetime "actual_completion_at"
     t.datetime "assigned_at"
     t.datetime "deployed_at"
-    t.datetime "start_at"
     t.datetime "due_at"
-    t.datetime "actual_completion_at"
+    t.datetime "start_at"
     t.datetime "created_at",            :null => false
     t.datetime "updated_at",            :null => false
   end
@@ -107,34 +117,39 @@ ActiveRecord::Schema.define(:version => 20130207201638) do
   add_index "reviews", ["purchased_template_id"], :name => "index_reviews_on_purchased_template_id"
 
   create_table "tasks", :force => true do |t|
-    t.string   "name"
+    t.integer  "executor_id"
+    t.float    "completion_percentage"
     t.string   "instructions"
+    t.string   "name"
     t.integer  "review_id"
     t.integer  "sequence"
     t.string   "status"
-    t.datetime "assigned_at"
-    t.datetime "start_at"
-    t.datetime "expected_completion_at"
     t.datetime "actual_completion_at"
-    t.float    "completion_percentage"
+    t.datetime "assigned_at"
+    t.datetime "expected_completion_at"
+    t.datetime "start_at"
     t.datetime "created_at",             :null => false
     t.datetime "updated_at",             :null => false
   end
 
+  add_index "tasks", ["executor_id"], :name => "index_tasks_on_executor_id"
   add_index "tasks", ["review_id"], :name => "index_tasks_on_review_id"
   add_index "tasks", ["status"], :name => "index_tasks_on_status"
 
   create_table "templates", :force => true do |t|
+    t.float    "price"
     t.integer  "agency_id"
     t.string   "agency_display_name"
-    t.string   "display_name"
-    t.string   "full_name"
-    t.string   "frequency"
     t.text     "description"
+    t.string   "display_name"
+    t.string   "frequency"
+    t.string   "full_name"
     t.text     "objectives"
     t.string   "regulatory_review_name"
-    t.integer  "author_id"
     t.text     "tasks"
+    t.integer  "author_id"
+    t.text     "reasons_for_update"
+    t.integer  "parent_id"
     t.datetime "created_at",             :null => false
     t.datetime "updated_at",             :null => false
   end
@@ -146,13 +161,13 @@ ActiveRecord::Schema.define(:version => 20130207201638) do
   add_index "templates", ["full_name"], :name => "index_templates_on_full_name"
 
   create_table "users", :force => true do |t|
-    t.string   "language"
-    t.string   "phone"
     t.integer  "organization_id"
+    t.string   "display_name"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "display_name"
+    t.string   "phone"
     t.integer  "profile_image_id"
+    t.string   "language"
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
