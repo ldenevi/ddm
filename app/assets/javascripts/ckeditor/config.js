2d8e3bec8e527cd317781f2a79b1f6c5e84cd969
Leo de Nevi <leonardo.denevi@greenstatuspro.com>
2013-03-14 13:44:49 -0400

    Added list & liststyle CKEditor plugins

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -5,6 +5,24 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	// config.language = 'it';
+	config.uiColor = '#007C43';
+	
+	// Toolbar configuration generated automatically by the editor based on config.toolbarGroups.
+  config.toolbar = [
+	  { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'NewPage' ] },
+	  { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+	  { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+	  { name: 'paragraph', groups: [ 'list', 'liststyle', 'indent' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent' ] },
+	  '/',
+	  { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+	  { name: 'styles', items: [ 'Font', 'FontSize' ] },
+	  { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+	  '/',
+	  { name: 'links', items: [ 'Link', 'Unlink' ] },
+	  { name: 'insert', items: [ 'Smiley', 'SpecialChar' ] },
+	  { name: 'others', items: [ '-' ] },
+	  { name: 'about', items: [ 'About' ] }
+  ];
+
 };
