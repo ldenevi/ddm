361686e0f74f94d394fb0dde59ce64697d95501b
Leo de Nevi <leonardo.denevi@greenstatuspro.com>
2013-03-27 22:53:39 -0400

    Highlight **Admin** link in menu when in admin features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   def active_feature(menu_item)
-    'top-menu-link-active' if params[:controller] == menu_item;
+    if (params[:controller].match(/^admin/).to_s == menu_item)
+      'top-menu-link-active'
+    elsif params[:controller] == menu_item
+      'top-menu-link-active'
+    else
+      ''
+    end
   end
 end
