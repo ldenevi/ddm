ffb251861880ca1ebf78a8fd9def1d004c06e438
Leo de Nevi <leonardo.denevi@greenstatuspro.com>
2013-05-09 21:58:12 -0400

    Updated model names in tests

--- spec/models/gsp_template_spec.rb
+++ spec/models/gsp_template_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe GSPTemplate do
+describe GspTemplate do
   describe 'data validations' do
     it 'should prevent hazardous field values' do
       pending 'must have purchased data, owner, and organization'
