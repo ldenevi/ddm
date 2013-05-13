ca83c10b146bd93794cdafa725ab6bdcb837055e
Leo de Nevi <leonardo.denevi@greenstatuspro.com>
2013-05-13 11:36:55 -0400

    moved notification/reviews to notifications

--- app/mailers/notifications/reviews.rb
+++ app/mailers/notifications/reviews.rb
@@ -1,5 +1,5 @@
 class Notifications::Reviews < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "review@greenstatuspro.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
