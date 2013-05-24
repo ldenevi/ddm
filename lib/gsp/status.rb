441fb9817419bde562d2e6b6a3dc7c831ed7165d
Leo de Nevi <leonardo.denevi@greenstatuspro.com>
2013-05-24 11:13:01 -0400

    Task statuses

--- lib/gsp/status.rb
+++ lib/gsp/status.rb
@@ -3,4 +3,12 @@ module GSP::STATUS
   ACTIVE    = 'Active'   # After expected start date, not yet completed
   COMPLETED = 'Green'    # Done 
   PAST_DUE  = 'Past Due' # After expected completion date
+  
+  module TASK
+    INACTIVE   = 'Inactive'   # Before targeted start date, but before review is displayed in UI
+    ACTIVE     = 'Active'     # After targeted start date, not yet completed
+    CONFORMING = 'Conforming' # Completed and conforming to instructions
+    NON_CONFORMING = 'Non-Conforming' # Completed and not conforming to instructions
+    PAST_DUE   = 'Past Due'   # After expected completion date and not completed (conforming or non-conforming)
+  end
 end
