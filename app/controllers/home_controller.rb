9d8c2fa19da63e5d00c26b43ae36fd53f6df013b
Leo de Nevi <leonardo.denevi@greenstatuspro.com>
2013-06-04 11:34:48 -0400

    Re-enable home#index to access task worksheet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,11 @@
 class HomeController < ApplicationController
   def index
-=begin
     @user = current_user
     @active_tasks = current_user.active_tasks
     @recently_completed_tasks = current_user.recently_completed_tasks
     @active_reviews = current_user.active_reviews
     @upcoming_reviews = current_user.upcoming_reviews
     @past_due_reviews = current_user.past_due_reviews
-=end
   end
 
   # TODO add methods to Task and Review models for these...
