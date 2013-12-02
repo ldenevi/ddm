e1f7377a639e2b4ac93575a26d1311791f94a0a9
Leo de Nevi <leo.denevi@greenstatuspro.com>
2013-12-02 15:43:04 -0500

    Staging environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,19 +13,32 @@ set :ssh_options, { :forward_agent => true }
 default_run_options[:pty] = true
 
 
-# Servers
-set :gsp_app1, "50.97.148.131"
-set :gsp_db1, "50.97.148.133"
+# Menu options for targeted machines
+unles fetch(:stage, nil)
+  set :stage do
+    Capistrano::CLI.ui.choose do |menu|
+      menu.header = "Deploy to which server?"
+      menu.choice = "staging"
+      menu.choice = "production"
+      menu.prompt = "Server: "
+    end
+  end
+end
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :rails_env, :stage
+set :database_ip, "50.97.148.133"
 
-role :web, gsp_app1                      # Your HTTP server, Apache/etc
-role :app, gsp_app1                      # This may be the same as your `Web` server
-# role :db,  gsp_db1, :primary => true     # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
+case rails_env
+  when "staging"
+    role :web, "75.126.142.146"
+    role :app, "75.126.142.146"
+    role :db, database_ip
+  when "production"
+    role :web, "50.97.148.131"
+    role :app, "50.97.148.131"
+    role :db, database_ip
+end
 
-set :rails_env, :production
 
 set :deploy_to, "/var/www_rails/gsp-app"
 set :deploy_via, :export
@@ -55,18 +68,3 @@ namespace :log do
     run "tail -n100 #{deploy_to}/current/log/production.log"
   end
 end
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
