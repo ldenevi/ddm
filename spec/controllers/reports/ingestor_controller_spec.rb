328ed25e6458ca74dd53b442ba1f90a83d6f1c04
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-01-26 19:29:10 -0500

    Consolidated smelters tests

--- spec/controllers/reports/ingestor_controller_spec.rb
+++ spec/controllers/reports/ingestor_controller_spec.rb
@@ -1,12 +1,28 @@
 require 'spec_helper'
 
 describe Reports::IngestorController do
+  before(:each) do
+    sign_in FactoryGirl.create(:admin) # Using factory girl as an example
+  end
+
+  let(:batch) do
+    batch = Eicc::BatchValidationStatus.create.from_xml File.read(File.join(File.dirname(__FILE__), "ingestor_batch.xml"))
+    batch.update_attribute(:user_id, subject.current_user.id)
+    batch
+  end
+
+  it "should have current_user" do
+    subject.current_user.should_not be_nil
+  end
+
   describe "Consolidated Smelters" do
-    it "should exists" do
-      get 'consolidated_smelters', :id => 1
+    it "should exist" do
+      get 'consolidated_smelters', :id => batch.id
       response.should be_success
+      response.header['Content-Type'].should eq "application/excel"
     end
   end
+
   describe "Aggregated Declarations" do
     it "should exists" do
       get 'aggregated_declarations', :id => 1
