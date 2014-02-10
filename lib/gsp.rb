169dcdf89b9e9636d5a8928ae22fe3cdc91744c1
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-02-10 11:57:18 -0500

    Ducktype hack to fick rest-client bug

--- lib/gsp.rb
+++ lib/gsp.rb
@@ -1,3 +1,8 @@
+class Hash
+  # https://github.com/rest-client/rest-client/issues/141
+  remove_method :read
+end
+
 module GSP
   autoload :Client, File.join('gsp', 'client')
   autoload :Eicc, File.join('gsp', 'eicc')
