59180c114d278428839290493c93660efbfcf4da
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-03-12 20:05:11 -0400

    Test whether e-mail's domain exists in database, if so redirect user to contact page, otherwise save and redirect to welcome

--- app/controllers/trial/sign_up_controller.rb
+++ app/controllers/trial/sign_up_controller.rb
@@ -1,8 +1,13 @@
 class Trial::SignUpController < Trial::PublicController
   def form
+    @user = User.new_trial
   end
 
   def register_new_user
+    redirect_to :action => 'already_registered' if is_domain_already_registered(params[:user][:email])
+    user = User.new_trial(params[:user])
+    user.save
+    render :text => user.errors.inspect
   end
 
   def already_registered
@@ -18,6 +23,7 @@ class Trial::SignUpController < Trial::PublicController
   end
 
 private
-  def is_domain_already_registered
+  def is_domain_already_registered(email)
+    !User.where("email LIKE ?", "%@#{email.split("@").last}").empty?
   end
 end
