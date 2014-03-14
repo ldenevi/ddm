3e692b8a89352ab218a8a271a6eec826feae43b6
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-03-14 01:21:00 -0400

    Removed trial user related code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,4 @@
 require 'spec_helper'
 
 describe User do
-  let(:trial) { User.new_trial }
-
-  context "in general" do
-    it "should be able to create trial users" do
-      User.should respond_to(:new_trial)
-    end
-  end
-
-  context "as trial" do
-    it "should have the date the trial was created" do
-      trial.should respond_to(:trial_created_at)
-      trial.trial_created_at.should_not be_nil
-    end
-  end
 end
