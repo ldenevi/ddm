2dd5d71f68053fd406c37cdb9a97448d999130b7
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-03-15 03:34:03 -0400

    Remove meaningless tests

--- spec/controllers/organization_controller_spec.rb
+++ spec/controllers/organization_controller_spec.rb
@@ -1,26 +1,4 @@
 require 'spec_helper'
 
 describe OrganizationController do
-
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'hierarchy'" do
-    it "returns http success" do
-      get 'hierarchy'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'overview'" do
-    it "returns http success" do
-      get 'overview'
-      response.should be_success
-    end
-  end
-
 end
--- spec/controllers/review_controller_spec.rb
+++ spec/controllers/review_controller_spec.rb
@@ -1,12 +1,4 @@
 require 'spec_helper'
 
 describe ReviewController do
-
-  describe "GET 'show_task'" do
-    it "returns http success" do
-      get 'show_task'
-      response.should be_success
-    end
-  end
-
 end
