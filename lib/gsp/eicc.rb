878f01b668712cb46c5fc58e3de9680b311ac6c1
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-03-17 15:35:34 -0400

    Add trial limitation configuration

--- lib/gsp/eicc.rb
+++ lib/gsp/eicc.rb
@@ -2,4 +2,8 @@ module GSP::Eicc
   autoload :Excel, File.join('gsp', 'eicc', 'excel')
   autoload :Exceptions, File.join('gsp', 'eicc', 'exceptions')
   autoload :Versions, File.join('gsp', 'eicc', 'versions')
+
+  Rails.configuration.trial.cfsi.max_batches = 3
+  Rails.configuration.trial.cfsi.max_declaration_uploads = 5
+  Rails.configuration.trial.cfsi.max_suppliers = 150
 end
