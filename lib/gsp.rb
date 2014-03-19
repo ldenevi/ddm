6054705cc74894c704e9ac14193333f983041bbe
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-03-19 10:44:18 -0400

    Added trial library, bumped versio nto 0.4.7

--- lib/gsp.rb
+++ lib/gsp.rb
@@ -9,10 +9,11 @@ module GSP
   autoload :FileManager, File.join('gsp', 'file_manager')
   autoload :Reports, File.join('gsp', 'reports')
   autoload :STATUS, File.join('gsp', 'status')
+  autoload :Trial, File.join('gsp', 'trial')
   autoload :UI, File.join('gsp', 'ui')
   autoload :Users, File.join('gsp', 'users')
 
-  VERSION = '0.4.508'
+  VERSION = '0.4.7'
 
   class Application < Rails::Application
     include GSP::STATUS
