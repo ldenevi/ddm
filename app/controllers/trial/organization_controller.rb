94388b70be7ee1171252053f114feab840fd559c
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-03-19 10:41:10 -0400

    Redirect to root_url after creating organization

--- app/controllers/trial/organization_controller.rb
+++ app/controllers/trial/organization_controller.rb
@@ -6,11 +6,9 @@ class Trial::OrganizationController < ApplicationController
   def create
     @organization = Organization.new params[:organization]
     current_user.organization = @organization
-
-    puts current_user.inspect
-
     if @organization.save && current_user.save
       flash[:notice] = {:success => ["Created #{@organization.full_name}"]}
+      redirect_to root_url
     else
       flash[:alert] = @organization.errors.messages.merge(current_user.errors.messages)
       redirect_to :back
