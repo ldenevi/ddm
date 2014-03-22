d1b261faf9cfb8474d0847d7df9f8e583598c382
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-03-22 03:02:50 -0400

    Deploy email

--- app/mailers/notifications/reviews.rb
+++ app/mailers/notifications/reviews.rb
@@ -10,7 +10,15 @@ class Notifications::Reviews < ActionMailer::Base
     @greeting = "E-Mail test"
     mail :to => "leo.denevi@greenstatuspro.com", :subject => "[TEST] Review #{review.name} deployed" #email
   end
-  
+
   def task_comment_added
+  end
+
+  def deploy(review)
+    coordinator = review.responsible_party
+    delegates   = review.tasks.collect(&:reviewer).uniq
+    subject     = "New review available [due #{review.targeted_completion_at.strftime("%b %d, %Y")}]"
+    @review     = review
+    mail :to => [coordinator.email, delegates.collect(&:email)].flatten.uniq, :subject => subject
   end
 end
