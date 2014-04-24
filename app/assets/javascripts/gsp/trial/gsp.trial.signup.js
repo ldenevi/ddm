a2b63ada5d3bfbd918b0890d20a0971a97e708a9
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-04-24 10:55:55 -0400

    Client-side email validation

--- app/assets/javascripts/gsp/trial/gsp.trial.signup.js
+++ app/assets/javascripts/gsp/trial/gsp.trial.signup.js
@@ -1,4 +1,7 @@
-$("#email").keyup(function(event) {
+var email_text_field = $("#email");
+var sign_up_form     = $("#sign_up_form");
+
+email_text_field.keyup(function(event) {
   var field = event.target;
   if (validateEmail(event.target.value))
     $(field).removeClass("invalid-email");
@@ -7,6 +10,21 @@ $("#email").keyup(function(event) {
 });
 
 function validateEmail(address) {
+  if (address.length == 0)
+    return false;
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(address);
 }
+
+sign_up_form.submit(function(event) {
+  var addr     = email_text_field.val();
+  var is_valid = validateEmail(addr);
+
+  if (!is_valid) {
+    alert("Invalid email");
+    return false;
+  }
+  else {
+    return true;
+  }
+})
