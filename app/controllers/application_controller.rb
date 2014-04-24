b58937a14b2510f9630474fa43195c4d545e728e
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-04-24 11:58:59 -0400

    Redirect trial user to Ingestor after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,37 +7,41 @@ class ApplicationController < ActionController::Base
     return true if ENV['RAILS_ENV'] == 'test'
     redirect_to new_user_session_path if current_user.nil?
   end
-  
+
   def check_browser
     supported_browsers = {:chrome => 25, :firefox => 8, :ie => 10, :safari => 20, :opera => 8}
     useragent = UserAgent.parse(request.env["HTTP_USER_AGENT"])
     @is_browser_supported = (supported_browsers[useragent.browser.downcase.to_sym] && (useragent.version.to_a[0] >= supported_browsers[useragent.browser.downcase.to_sym]))
-    
+
     @is_browser_supported = true
     @browser_info = request
   end
-  
+
 protected
   def use_ckeditor
     @use_ckeditor = true
   end
 
   def after_sign_in_path_for(resource)
-    session[:return_to] || root_path
+    if current_user.is_a?(Trial::TrialUser)
+      eicc_declaration_index_path
+    else
+      session[:return_to] || root_path
+    end
   end
 
-private  
+private
   def store_path
     session[:return_to] = request.fullpath unless request.fullpath =~ /\/users/
   end
-  
+
   def clear_store_path
     session[:return_to] = nil
   end
-  
+
   def redirect_back_or_to(alternate)
     redirect_to(session[:return_to] || alternate)
     clear_stored_location
   end
-  
+
 end
