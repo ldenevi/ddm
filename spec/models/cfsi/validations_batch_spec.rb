0e0302b35425014afce437501a751bbafedac94a
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-06-15 04:34:22 -0400

    Test for Cfsi::ValidationsBatch#cmrt_validations

--- spec/models/cfsi/validations_batch_spec.rb
+++ spec/models/cfsi/validations_batch_spec.rb
@@ -4,7 +4,7 @@ describe Cfsi::ValidationsBatch do
   let(:batch) { FactoryGirl.create(:cfsi_validations_batch) }
   context "(in general)" do
     it "should have required attributes" do
-      [:unidentified_cmrts, :vendor_cmrts, :status, :user, :organization].each do |attr|
+      [:unidentified_cmrts, :vendor_cmrts, :status, :user, :organization, :cmrt_validations].each do |attr|
         expect(batch).to respond_to attr
       end
     end
