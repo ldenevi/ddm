7f31967bf910f04ae27a8245ab50d0397941b4c4
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-07-04 05:53:51 -0400

    Makes root_parent a Company, add admin user, and prevents saving if root_parent's division limit is exceeded

--- app/models/organizations/division.rb
+++ app/models/organizations/division.rb
@@ -1,2 +1,8 @@
 class Division < Organization
+  belongs_to :root_parent, :class_name => 'Company'
+  belongs_to :admin, :class_name => 'User', :foreign_key => 'owner_id'
+
+  validate do |record|
+    record.errors.add(:division, "limit exceeded (#{record.root_parent.division_limit})") if record.root_parent && record.root_parent.divisions.size >= record.root_parent.division_limit
+  end
 end
