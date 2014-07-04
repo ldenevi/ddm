4df8814904612be07302a77edeb09ae8ac6ff768
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-07-04 06:11:58 -0400

    Validate for presence of a user

--- app/models/organizations/division.rb
+++ app/models/organizations/division.rb
@@ -1,6 +1,7 @@
 class Division < Organization
   belongs_to :root_parent, :class_name => 'Company'
   belongs_to :admin, :class_name => 'User', :foreign_key => 'owner_id'
+  validate :users, :presence => true
 
   validate do |record|
     record.errors.add(:division, "limit exceeded (#{record.root_parent.division_limit})") if record.root_parent && record.root_parent.divisions.size >= record.root_parent.division_limit
