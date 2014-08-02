7c5c23c86530bb0c85c00e557eb72464efcb4cfd
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-08-02 15:36:26 -0400

    Add version CMRT 3.00 support

--- lib/gsp/protocols/regulations/cfsi/cmrt/maps.rb
+++ lib/gsp/protocols/regulations/cfsi/cmrt/maps.rb
@@ -4,6 +4,7 @@ module GSP::Protocols::Regulations::CFSI::CMRT::Maps
   autoload :Version2Dot02, File.join('gsp', 'protocols', 'regulations', 'cfsi', 'cmrt', 'maps', 'version_2.02')
   autoload :Version2Dot03, File.join('gsp', 'protocols', 'regulations', 'cfsi', 'cmrt', 'maps', 'version_2.03')
   autoload :Version2Dot03a, File.join('gsp', 'protocols', 'regulations', 'cfsi', 'cmrt', 'maps', 'version_2.03a')
+  autoload :Version3Dot00, File.join('gsp', 'protocols', 'regulations', 'cfsi', 'cmrt', 'maps', 'version_3.00')
   autoload :Version3Dot01, File.join('gsp', 'protocols', 'regulations', 'cfsi', 'cmrt', 'maps', 'version_3.01')
 
   def self.included(obj)
