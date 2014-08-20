8ae891e4012848a1e15d618026602e181ab983d2
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-08-20 16:44:13 -0400

    Notify GSP when new TrialUser registers

--- app/mailers/notifications/security.rb
+++ app/mailers/notifications/security.rb
@@ -6,4 +6,9 @@ class Notifications::Security < ActionMailer::Base
     @user = user
     mail :to => user.email, :subject => "Welcome to Green Status Pro!"
   end
+
+  def notify_gsp_of_new_trial_user(user)
+    @user = user
+    mail :to => "leo.denevi@greenstatuspro.com, john.logan@greenstatuspro.com, rob.kasameyer@greenstatuspro.com, steven.correia@greenstatuspro.com, jack.logan@greenstatuspro.com", :subject => "New trial user signed up"
+  end
 end
