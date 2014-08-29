d32dab71df10ad441ebb6c1ecba6021f55aaf021
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-08-29 11:38:28 -0400

    Update the consolidated counts

--- spec/lib/protocols/regulations/cfsi/reports/excel/consolidated_smelters_spec.rb
+++ spec/lib/protocols/regulations/cfsi/reports/excel/consolidated_smelters_spec.rb
@@ -88,8 +88,8 @@ describe GSP::Protocols::Regulations::CFSI::Reports::Excel::ConsolidatedSmelters
   it "should group smelters" do
     batch = Cfsi::ValidationsBatch.new(:unidentified_cmrt_validations => [Cfsi::CmrtValidation.new(:cmrt => Cfsi::Cmrt.new(:declaration => Cfsi::Declaration.new(:mineral_smelters => smelters)))])
     csr   = GSP::Protocols::Regulations::CFSI::Reports::Excel::ConsolidatedSmelters.new(batch)
-    expect(csr.consolidated_smelters[:data].size).to eq 9
-    expect(csr.rejected_entries[:data].size).to eq 14
+    expect(csr.consolidated_smelters[:data].size).to eq 7
+    expect(csr.rejected_entries[:data].size).to eq 23
   end
 
   it "should associate consolidated smelter data only to sourced CMRTs of said smelter" do
