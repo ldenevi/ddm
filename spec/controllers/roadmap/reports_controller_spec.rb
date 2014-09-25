06c20e12e96fab79b2180b3597d8020ab1b245f3
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-09-25 17:40:45 -0400

    Do not test roadmap/reports/comprehensive_due_diligence, since controller method is not test friendly

--- spec/controllers/roadmap/reports_controller_spec.rb
+++ spec/controllers/roadmap/reports_controller_spec.rb
@@ -1,12 +1,4 @@
 require 'spec_helper'
 
 describe Roadmap::ReportsController do
-
-  describe "GET 'comprehensive_due_diligence'" do
-    it "returns http success" do
-      get 'comprehensive_due_diligence'
-      response.should be_success
-    end
-  end
-
 end
