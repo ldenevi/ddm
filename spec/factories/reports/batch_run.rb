d82ee50f1da775639d7928ff13e1a670d8b5e69c
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-09-25 22:27:16 -0400

    Moved :admin to user_spec.rb

--- spec/factories/reports/batch_run.rb
+++ spec/factories/reports/batch_run.rb
@@ -1,15 +1,6 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :admin, :class => 'User' do
-    first_name "Test"
-    last_name "User"
-    email "test@user.com"
-    password "password"
-    association :organization, :factory => :test_co
-    confirmed_at Time.now
-  end
-
   factory :test_co, :class => 'Organization' do
     name "ACME Test"
   end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,20 @@
 FactoryGirl.define do
+  sequence(:email) { |n| "test#{n}@user.com.ts" }
+  
   factory :user, :class => 'User' do
     first_name "Test"
     last_name "User"
-    email "test@user.com.ts"
+    email
     password "password"
     organization
+  end
+  
+  factory :admin, :class => 'User' do
+    first_name "Test"
+    last_name "User"
+    email
+    password "password"
+    association :organization, :factory => :test_co
+    confirmed_at Time.now
   end
 end
