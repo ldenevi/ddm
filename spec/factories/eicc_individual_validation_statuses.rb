66872548515f95e6dd4d8e6bae949c7729ee9472
Leo de Nevi <leo.denevi@greenstatuspro.com>
2014-09-30 15:21:33 -0400

    Remove outdated EICC files

--- spec/factories/eicc_individual_validation_statuses.rb
+++
@@ -1,6 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :eicc_individual_validation_status, :class => 'Eicc::IndividualValidationStatus' do
-  end
-end
